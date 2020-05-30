563680288ca33139942ef4c130e28c3729ae48da
Lucidreline <techservs@pm.me>
2020-05-29 22:45:01 -0700

    Created fake inventory data

---
+++ src/shop-Data.js
@@ -0,0 +1,235 @@
+const SHOP_DATA = [
+  {
+    title: 'Designs',
+    routeName: 'designs',
+    items: [
+      {
+        name: 'Blue Bike',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812427/Sahx%20Shop/Products/designs/designs_5_olfumz.jpg',
+        price: 13,
+      },
+      {
+        name: 'Yellow Seeds',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812413/Sahx%20Shop/Products/designs/designs_1_anpgxo.jpg',
+        price: 15,
+      },
+      {
+        name: 'Blue Delight',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812410/Sahx%20Shop/Products/designs/designs_2_ixoc5c.jpg',
+        price: 15,
+      },
+      {
+        name: 'Futuristic',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812426/Sahx%20Shop/Products/designs/designs_6_s0gkds.jpg',
+        price: 16,
+      },
+      {
+        name: 'Lips',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812426/Sahx%20Shop/Products/designs/designs_4_mkitua.jpg',
+        price: 14,
+      },
+      {
+        name: 'Strawberry Mango',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812424/Sahx%20Shop/Products/designs/designs_9_h8ctgj.jpg',
+        price: 13,
+      },
+      {
+        name: 'Poppy Stripes',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812417/Sahx%20Shop/Products/designs/designs_7_twmweb.jpg',
+        price: 14,
+      },
+    ],
+  },
+  {
+    title: 'Dark',
+    routeName: 'dark',
+    items: [
+      {
+        name: 'Dark Stripes',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812380/Sahx%20Shop/Products/dark/dark_9_ryajfd.jpg',
+        price: 15,
+      },
+      {
+        name: 'Pitch Black',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812389/Sahx%20Shop/Products/dark/dark_6_gbwt3d.jpg',
+        price: 13,
+      },
+      {
+        name: 'Kool Kids',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812379/Sahx%20Shop/Products/dark/dark_5_rprjq2.jpg',
+        price: 14,
+      },
+      {
+        name: "Feet's Home",
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812381/Sahx%20Shop/Products/dark/dark_3_nbqixe.jpg',
+        price: 14,
+      },
+      {
+        name: 'Scribles',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812383/Sahx%20Shop/Products/dark/dark_1_igj3u2.jpg',
+        price: 14,
+      },
+      {
+        name: 'Flexable Flex',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812385/Sahx%20Shop/Products/dark/dark_4_zvtkxz.jpg',
+        price: 14,
+      },
+    ],
+  },
+  {
+    title: 'Fluffy',
+    routeName: 'fluffy',
+    items: [
+      {
+        name: 'Snuggly Bunny',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812446/Sahx%20Shop/Products/fluffy/fluffy_2_pk1asg.jpg',
+        price: 17,
+      },
+      {
+        name: 'Grey Cotton',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812443/Sahx%20Shop/Products/fluffy/fluffy_1_wdhcur.jpg',
+        price: 16,
+      },
+      {
+        name: 'Walking on Clouds',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812443/Sahx%20Shop/Products/fluffy/fluffy_3_ozbwiq.jpg',
+        price: 20,
+      },
+      {
+        name: 'Warm Hug',
+        imageUrl: '',
+        price: 0,
+      },
+      {
+        name: 'Feet Mittens',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812441/Sahx%20Shop/Products/fluffy/fluffy_7_pwg4hx.jpg',
+        price: 16,
+      },
+    ],
+  },
+  {
+    title: 'Light',
+    routeName: 'light',
+    items: [
+      {
+        name: 'Casual Whites',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812464/Sahx%20Shop/Products/light/light_3_xjz3gj.jpg',
+        price: 14,
+      },
+      {
+        name: 'Gold Wrap',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812459/Sahx%20Shop/Products/light/light_5_surmbd.jpg',
+        price: 16,
+      },
+      {
+        name: 'Clean Slims',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812459/Sahx%20Shop/Products/light/light_8_wuhluq.jpg',
+        price: 14,
+      },
+      {
+        name: 'Subtle Lows',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812458/Sahx%20Shop/Products/light/light_9_itltvo.jpg',
+        price: 14,
+      },
+      {
+        name: 'Poppin Yellows',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812458/Sahx%20Shop/Products/light/light_2_tivyj0.jpg',
+        price: 15,
+      },
+      {
+        name: 'Blue Splash',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812456/Sahx%20Shop/Products/light/light_4_lxymli.jpg',
+        price: 16,
+      },
+      {
+        name: "Clean Vetem's",
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812454/Sahx%20Shop/Products/light/light_6_t4deyd.jpg',
+        price: 15,
+      },
+      {
+        name: 'Stretchy Greatness',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812454/Sahx%20Shop/Products/light/light_7_sgtqnn.jpg',
+        price: 16,
+      },
+      {
+        name: 'LA Simplified',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812453/Sahx%20Shop/Products/light/light_1_at7jqq.jpg',
+        price: 16,
+      },
+    ],
+  },
+  {
+    title: 'Plane',
+    routeName: 'plane',
+    items: [
+      {
+        name: 'clean Canvas',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812477/Sahx%20Shop/Products/plane/plane_3_wsadki.jpg',
+        price: 14,
+      },
+      {
+        name: 'Dark Hole',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812476/Sahx%20Shop/Products/plane/plane_1_qod9nh.jpg',
+        price: 13,
+      },
+      {
+        name: 'Blue Stripe',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812475/Sahx%20Shop/Products/plane/plane_8_r05qfr.jpg',
+        price: 15,
+      },
+      {
+        name: 'Colorful Stripe',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812474/Sahx%20Shop/Products/plane/plane_5_enmsxq.jpg',
+        price: 14,
+      },
+      {
+        name: 'Bright Greens',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812470/Sahx%20Shop/Products/plane/plane_6_sa1ujo.jpg',
+        price: 14,
+      },
+      {
+        name: 'Classoc Ankle White',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812469/Sahx%20Shop/Products/plane/plane_7_v8ha48.jpg',
+        price: 12,
+      },
+      {
+        name: 'Low Greens',
+        imageUrl:
+          'https://res.cloudinary.com/dglxjd018/image/upload/v1590812468/Sahx%20Shop/Products/plane/plane_9_hgoyc3.jpg',
+        price: 13,
+      },
+    ],
+  },
+];
